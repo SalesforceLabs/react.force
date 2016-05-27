979d5d893c51b060f3058a5088b5c276ac76a2f3
ibogdanov <ibogdanov@salesforce.com>
2016-05-26 18:25:05 -0700

    convenience

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   net:require('./src/react.force.net.js'),
   oauth:require('./src/react.force.oauth.js'),
   smartstore:require('./src/react.force.smartstore.js'),
-  smartsync:require('./src/react.force.smartsync.js')
+  smartsync:require('./src/react.force.smartsync.js'),
+  forceClient:require('./src/react.force.net.js')
 };
