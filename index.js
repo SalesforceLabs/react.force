44d0d12ee5997daaf9e78ec9f18aec752bddc3fa
ibogdanov <ibogdanov@salesforce.com>
2016-05-26 18:01:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  net:require('./react.force.net.js'),
+  oauth:require('./react.force.oauth.js'),
+  smartstore:require('./react.force.smartstore.js'),
+  smartsync:require('./react.force.smartsync.js')
+};
